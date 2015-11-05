642b10bf2bbb7d9eaf10429bc17d0234cab63008
Chance Smith <chance@sodiumhalogen.com>
2015-11-05 09:35:16 -0600

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# trophy-countdown
+# CO:de Catalyst Championship Trophy Countdown
 Arduino project to countdown seconds to event date.
+
+
+##Heads-up
+The time will restart once powered down. You'll need a battery source (like we did), or install a RTC chip.
---
+++ trophy-project/trophy-countdown.ino
@@ -0,0 +1,33 @@
+#include <LiquidCrystal.h>
+#include <Time.h>
+#include <Wire.h>
+#include <DS1307RTC.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  //clear screen
+  lcd.clear();
+  // Print a message to the LCD.
+  lcd.print("COde champions");
+  lcd.setCursor(0, 1);
+  lcd.print("until ");
+}
+
+void loop () {
+
+
+  setSyncProvider(RTC.get); //need RTC chip installed to work. Otherwise, just counting down secs.
+
+  time_t timeLeft,eventDate; // setup the fields needed.
+       eventDate = 12732290; // April 01, 2016, in seconds until RTC chip is installed
+       timeLeft = eventDate - now();
+
+      // Display the seconds until event.
+      lcd.setCursor(6, 1);
+      lcd.print(timeLeft);
+      delay(1000);
+}
